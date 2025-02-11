d17a13ef6910d45a8eddf140969b39ddfe718855
Irving2355 <it235597@gmail.com>
2025-02-11 17:47:54 -0600

    Clase 11/02/2025

--- funciones1.c
+++ funciones1.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int vec[10]={1,2,3,4,5,6,7,8,9,10};
+void imprimir();
 
+int main(void){
+    imprimir();
     printf("\n");
     system("pause");
     return 0;
+}
+
+void imprimir(){
+    int tam =sizeof(vec)/sizeof(vec[0]);//vec[0]= 4= int
+    for(int i=0; i<tam; i++){
+        printf("%d",vec[i]);
+    }
 }
