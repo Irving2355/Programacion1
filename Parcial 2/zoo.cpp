df76b46b6db2c8db9000cf2308ac5d0fd3d2fc55
Irving2355 <it235597@gmail.com>
2025-03-11 16:03:55 -0600

    Clase 11/03/2025

---
+++ Parcial 2/zoo.cpp
@@ -0,0 +1,21 @@
+/*
+Se desea almacenar los datos de N animales
+los cuales estan clasificados en domesticos,
+salvajes y acuaticos
+
+Se hara uso de struct enum y union
+*/
+
+#include <iostream>
+#include <iomanip>
+/*#include <iomanip> nos ayuda con herramientas 
+para formatear la salida de texto y números, 
+especialmente en lo que respecta a la alineación, 
+precisión decimal y presentación de valores.*/
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
