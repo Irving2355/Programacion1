dc0451b05cfae85018944d67e50624b0e1ff752b
Irving2355 <it235597@gmail.com>
2025-03-21 19:19:31 -0600

    Clase 21/03/2025

--- Parcial 2/Archivos/escribirPorCampo.cpp
+++ Parcial 2/Archivos/escribirPorCampo.cpp
@@ -7,5 +7,36 @@
 using namespace std;
 
 int main(){
+    fstream file;
+    char nomarch[30]="clientes.txt";
+    int cuenta;
+    string nombre;
+    float monto;
+    char resp='s';
+
+    //abre archvio de tipo escritura
+    file.open(nomarch, ios::out|ios::app);
+    if(!file)
+        cout << "no se abrio";
+    else{
+        while (resp == 's')
+        {
+            cout << "\n Dame numero de cuenta: ";
+            cin >> cuenta;
+            cout << "\n Dame nombre: ";
+            fflush(stdin);
+            getline(cin,nombre);
+            cout << "\n Dame el monto: ";
+            cin >> monto;
+
+            //escribe o guarda dentro del archivo
+            file << cuenta << " " << nombre << " " << monto << endl;
+
+            cout << "\n Otro mas s/n";
+            fflush(stdin);
+            cin.get(resp);
+        }
+        file.close();
+    }
     return 0;
 }
