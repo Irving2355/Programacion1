252e909f8cfa582b33741d943f2246b4cb00b5d3
Irving2355 <it235597@gmail.com>
2025-05-12 19:49:38 -0600

    Clase 09/05/2025

--- Parcial 3/matrizDinamica3.cpp
+++ Parcial 3/matrizDinamica3.cpp
@@ -17,7 +17,53 @@ using namespace std;
 
 int main(){
     srand(time(0));
-
     int **mat = totalrenycol(5,5);
+    llenar(mat, 5,5);
+    imprimir(mat, 5,5);
+    cout << endl << "ZigZag" << endl;
+    zigzag(mat, 5,5);
     return 0;
+}
+
+//crea una matriz vacia de enteros
+int **totalrenycol(int ren, int col){
+    int **matriz = new int*[ren];
+    for(int i=0; i<ren; i++){
+        matriz[i] = new int[col];
+    }
+    return matriz;
+}
+
+void llenar(int **matriz, int ren, int col){
+    for(int i=0; i<ren; i++){
+        for(int j=0; j<col; j++){
+            matriz[i][j] = 1 + rand()%50;
+        }
+    }
+}
+void imprimir(int **matriz, int ren, int col){
+    for(int i=0; i<ren; i++){
+        for(int j=0; j<col; j++){
+            cout << *(*(matriz + i) + j) << " ";
+        }
+        cout << endl;
+    }
+}
+
+void zigzag(int **matriz, int ren, int col){
+    for(int i=0; i<ren; i++){
+        if(i % 2 == 0){
+            //fila va de izq a der
+            for(int j=0; j<col; j++){
+                cout << setw(4) << right << matriz[i][j];
+            }
+        }else{
+            //fila der a izq
+            //5-1  
+            for(int j=col-1; j>=0; j--){
+                cout << setw(4) << left << matriz[i][j];
+            }
+        }
+        cout << endl;
+    }
 }
