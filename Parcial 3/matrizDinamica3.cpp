da91ab7ce4509a1d31b426d042d99e1ba6c32bf4
Irving2355 <it235597@gmail.com>
2025-05-13 19:50:50 -0600

    Clase 13/05/2025

--- Parcial 3/matrizDinamica3.cpp
+++ Parcial 3/matrizDinamica3.cpp
@@ -22,6 +22,17 @@ int main(){
     imprimir(mat, 5,5);
     cout << endl << "ZigZag" << endl;
     zigzag(mat, 5,5);
+
+    //Registro de nombres matriz char
+    char **matriz = new char*[5];
+    cout << endl;
+    llenarnom(matriz, 5);
+    imprimirnom(matriz,5);
+
+    char *estudiante = new char[50];
+    cout << "Dame un nombre a buscar: ";
+    cin.getline(estudiante,50);
+    imprimirestudiantes(matriz, 5, estudiante);
     return 0;
 }
 
@@ -65,5 +76,46 @@ void zigzag(int **matriz, int ren, int col){
             }
         }
         cout << endl;
+    }
+}
+
+void llenarnom(char **matriz, int ren){
+    char nom[50];
+    for(int i=0; i<ren; i++){
+        cout << "Dame un nombre: ";
+        cin.getline(nom,50);
+
+        int len = strlen(nom);
+        *(matriz + i) = new char[len];
+        for(int j=0; j<=len; j++){
+            *(*(matriz + i) +j) = nom[j];
+        }
+    }
+}
+
+void imprimirnom(char **matriz, int ren){
+    const int ancho = 10;
+
+    for(int i=0; i<ren; i++){
+        int len = strlen(matriz[i]);
+        int espaciosIzq = (ancho - len) / 2;
+
+        cout << setw(espaciosIzq + len) << matriz[i] << endl;
+    }
+}
+
+void imprimirestudiantes(char **matriz, int ren, char *estudiante){
+    bool encontrado = false;
+
+    for(int i=0; i<ren; i++){
+        if(strcmp(*(matriz + i), estudiante) == 0){
+            cout << "Nombre registrado" << endl;
+            encontrado = true;
+            break;
+        }
+    }
+
+    if(!encontrado){ //if(encontrado == false)
+        cout << "Nombre no registrado" << endl;
     }
 }
